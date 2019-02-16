9367c8a5de1c0a5d2e57f68c7cd4408cbfa672ff
guilhermeaeme <guilhermeaeme@gmail.com>
2019-02-16 11:36:37 -0200

    in progress

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -73,32 +73,34 @@ window.onload = () => {
 
   directionsDisplay.setMap(map);
   */
-};
 
-const geocoder = new google.maps.Geocoder();
+  if(document.getElementById('getLatLng')){
+    const geocoder = new google.maps.Geocoder();
 
-document.getElementById('getLatLng').addEventListener('click', function () {
-  // geocodeAddress(geocoder, map);
-  geocodeAddress(geocoder);
-});
-
-function geocodeAddress(geocoder, resultsMap) {
-  let address = document.getElementById('address').value;
-
-  geocoder.geocode({ 'address': address }, function (results, status) {
-
-    if (status === 'OK') {
-      /*
-      resultsMap.setCenter(results[0].geometry.location);
-      let marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
+    document.getElementById('getLatLng').addEventListener('click', function () {
+      // geocodeAddress(geocoder, map);
+      geocodeAddress(geocoder);
+    });
+    
+    function geocodeAddress(geocoder, resultsMap) {
+      let address = document.getElementById('address').value;
+    
+      geocoder.geocode({ 'address': address }, function (results, status) {
+    
+        if (status === 'OK') {
+          /*
+          resultsMap.setCenter(results[0].geometry.location);
+          let marker = new google.maps.Marker({
+            map: resultsMap,
+            position: results[0].geometry.location
+          });
+          */
+          document.getElementById('latitude').value = results[0].geometry.location.lat();
+          document.getElementById('longitude').value = results[0].geometry.location.lng();
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
       });
-      */
-      document.getElementById('latitude').value = results[0].geometry.location.lat();
-      document.getElementById('longitude').value = results[0].geometry.location.lng();
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
     }
-  });
-}
+  }
+};
